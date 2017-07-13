dd31e61b617152e85db52584910dbd65017cf94e
Mehmet Akyuz <mehmet@mehmetakyuz.net>
2017-07-13 13:27:50 +0200

    Fix bids line

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ class WebhookHandler(webapp2.RequestHandler):
                     if text.split(' ')[1] == 'book':
                         order_book = kraken.getOrderBook(pair=ASSETPAIRS[pair])
                         book = order_book[ASSETPAIRS[pair]]
-                        r = "*OrderBook* {0} \n*Asks*\n{1}\n\n*Bids*ln{2}".format(
+                        r = "*OrderBook* {0} \n*Asks*\n{1}\n\n*Bids*\n{2}".format(
                             ASSETPAIRS[pair],
                             "\n".join(
                                 ["{} {}".format(ask[0], ask[1]) for ask in book['asks'][:10]]

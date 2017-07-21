5258ebdbab71693ebef2dfe5c66bf82e65a8193d
mehmetakyuz <mehmet@mehmetakyuz.net>
2017-07-21 13:35:14 +0200

    weak hands

--- main.py
+++ main.py
@@ -286,6 +286,9 @@ class WebhookHandler(webapp2.RequestHandler):
             reply('dont forget to bring a towel')
         elif 'penny' in text:
             reply('Dont talk behind my back!')
+        elif 'selling' in text or 'dropping' in text:
+            if random.choice([True, False]):
+                reply('weak hands!')
         elif 'what time' in text:
             reply('look at the corner of your screen!')
         elif 'moon' in text:

60760ded0135fb636e1eac3a06fcffa6231c375c
Bastien Boutonnet <hello@bastienboutonnet.com>
2017-08-17 23:09:40 +0200

    added bittrex exchange

--- bittrex_playground.py
+++ bittrex_playground.py
@@ -10,7 +10,7 @@ import re
 
 import requests
 
-
+from bittrex_params import BITT_ASSETPAIRS
 
 def _query(url, header):
     r = requests.post(url, data=header)
@@ -34,11 +34,6 @@ BITT_PUBLIC_URLS = {
     'history': 'https://bittrex.com/api/v1.1/public/getmarkethistory'
 }
 
-BITT_ASSETPAIRS = {
-    'btc-ltc': 'btc-ltc',
-    'neo-btc': 'neo-btc'
-}
-
 BITT_TICKER_MAPPING = {
     'MarketName': 'Pair',
     'High': 'High',
@@ -51,6 +46,8 @@ BITT_TICKER_MAPPING = {
     'OpenBuyOrders': '# Buy Orders',
     'OpenSellOrders': '# Sell Orders'
 }
+
+
 
 # TODO: retrieve all pairs from the `getmarket` data. Pairs will have "-"
 #       which will be handy for separation.
@@ -84,14 +81,24 @@ class BittrexExchange(object):
                 self.ticker[BITT_TICKER_MAPPING[t]] = ticker[t]
         return self.ticker
 
+    def getmarkets(self, type, header=None):
+        header = None
+        r = self.query_public('markets', header)
+        self.markets = []
+        markets = r
+        for i, cont in enumerate(markets):
+            self.markets.append(markets[i]["MarketName"])
+        return self.markets
+
+
 
 def main(text, bot_on=False):
     if text.startswith('/'):
-        if text.split(' ')[0][1:] in BITT_ASSETPAIRS.keys():
+        if text.split(' ')[0][1:].upper() in BITT_ASSETPAIRS:
             # TODO: insert bittrex methods here
-            pair = text.split(' ')[0][1:]  # Actually upper is not needed here
+            pair = text.split(' ')[0][1:]
             bittrex = BittrexExchange()
-            ticker = bittrex.getTicker(pair=BITT_ASSETPAIRS[pair])
+            ticker = bittrex.getTicker(pair=pair)
 
             askPrice = float(ticker['Ask Price'])
             bidPrice = float(ticker['Bid Price'])
@@ -102,6 +109,7 @@ def main(text, bot_on=False):
 
             if len(text.split(' ')) > 1:
                 if text.split(' ')[1] == 'fib':
+                    print 'yo'
                     l_one = highPrice
                     l_two = highPrice - ((highPrice - lowPrice) * 0.236)
                     l_three = highPrice - ((highPrice - lowPrice) * 0.382)
@@ -113,13 +121,23 @@ def main(text, bot_on=False):
                     l_nine = highPrice - ((highPrice - lowPrice) * 1.618)
 
                     r = '*{0}* 24h fib levels\n\n*0%*: {1}\n*23.6%*: {2}\n*38.2%*: {3}\n*50%*: {4}\n*61.8%*: {5}\n*78.6%*: {6}\n*100%*: {7}\n\n*127.2%*: {8}\n*161.8%*: {9}\n'.format(pair, l_one, l_two, l_three, l_four, l_five, l_six, l_seven, l_eight, l_nine)
+                    print r
             else:
                 r = '*{}* \n*Price:* {} \n*---* \n*High:* {} \n*Low:* {}'.format(pair, price, highPrice, lowPrice)
-                if bot_on is True:
-                    reply(r)
-                else:
-                    return r
+            if bot_on is True:
+                reply(r)
+            else:
+                return r
+
+
+
 
 if __name__ == '__main__':
-    main('/neo-btc')
+    r=main('/btc-ltc fib')
+
+    
+
     r
+    r
+    len('/btc-ltc fib'.split(' '))
+    '/btc-ltc fib'.split(' ')[1]
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import re
 
 import requests
 import requests_toolbelt.adapters.appengine
+
+from bittrex_params import BITT_ASSETPAIRS
 # Use the App Engine Requests adapter. This makes sure that Requests uses
 # URLFetch.
 requests_toolbelt.adapters.appengine.monkeypatch()
@@ -280,19 +282,23 @@ class WebhookHandler(webapp2.RequestHandler):
                     r = '*{}* \n*Price:* {} \n*---* \n*High:* {} \n*Low:* {}'.format(pair, price, highPrice, lowPrice)
                 # r += '\n\n_updated: {}_'.format(time)
                 reply(r)
-            elif text.split(' ')[0][1:].upper() in BITT_ASSETPAIRS.keys():
+
+            elif text.split(' ')[0][1:].upper() in BITT_ASSETPAIRS:
                 # TODO: insert bittrex methods here
-                pair = text.split(' ')[0][1:].upper() # Actually upper is not needed here
+                pair = text.split(' ')[0][1:]
                 bittrex = BittrexExchange()
-                ticker = kraken.getTicker(pair=BITT_ASSETPAIRS[pair])
-                askPrice = float(ticker['Ask Price'][0])
-                bidPrice = float(ticker['Bid Price'][0])
+                ticker = bittrex.getTicker(pair=pair)
+
+                askPrice = float(ticker['Ask Price'])
+                bidPrice = float(ticker['Bid Price'])
                 price = (askPrice + bidPrice) / 2
-                highPrice = float(ticker['High'][0])
-                lowPrice = float(ticker['Low'][0])
+                highPrice = float(ticker['High'])
+                lowPrice = float(ticker['Low'])
                 r = ""
+
                 if len(text.split(' ')) > 1:
                     if text.split(' ')[1] == 'fib':
+                        print 'yo'
                         l_one = highPrice
                         l_two = highPrice - ((highPrice - lowPrice) * 0.236)
                         l_three = highPrice - ((highPrice - lowPrice) * 0.382)
@@ -304,9 +310,10 @@ class WebhookHandler(webapp2.RequestHandler):
                         l_nine = highPrice - ((highPrice - lowPrice) * 1.618)
 
                         r = '*{0}* 24h fib levels\n\n*0%*: {1}\n*23.6%*: {2}\n*38.2%*: {3}\n*50%*: {4}\n*61.8%*: {5}\n*78.6%*: {6}\n*100%*: {7}\n\n*127.2%*: {8}\n*161.8%*: {9}\n'.format(pair, l_one, l_two, l_three, l_four, l_five, l_six, l_seven, l_eight, l_nine)
+                        
                 else:
                     r = '*{}* \n*Price:* {} \n*---* \n*High:* {} \n*Low:* {}'.format(pair, price, highPrice, lowPrice)
-                    reply(r)
+                reply(r)
 
 
 
@@ -491,11 +498,6 @@ BITT_PUBLIC_URLS = {
     'history': 'https://bittrex.com/api/v1.1/public/getmarkethistory'
 }
 
-BITT_ASSETPAIRS = {
-    'btc-ltc': 'btc-ltc',
-    'neo-btc': 'neo-btc'
-}
-
 BITT_TICKER_MAPPING = {
     'MarketName': 'Pair',
     'High': 'High',
@@ -525,7 +527,7 @@ class BittrexExchange(object):
         super(BittrexExchange, self).__init__()
 
     def query_public(self, type, header=None):
-        return _query(PUBLIC_URLS[type], header)
+        return _query(BITT_PUBLIC_URLS[type], header)
 
     def getTicker(self, pair):
         header = {'market': pair} if pair else None
@@ -534,14 +536,21 @@ class BittrexExchange(object):
             return r.message
         self.ticker = {}
 
-        ticker = r
-        print(ticker['Last'])
+        ticker = r[0]
+        # print(ticker)
         for t in ticker.keys():
-            self.ticker[t] = ticker[t]
-
-        for t in ticker.keys():
-            self.ticker[BITT_TICKER_MAPPING[t]] = ticker[t]
+            if t in BITT_TICKER_MAPPING.keys():
+                self.ticker[BITT_TICKER_MAPPING[t]] = ticker[t]
         return self.ticker
+
+    def getmarkets(self, type, header=None):
+        header = None
+        r = self.query_public('markets', header)
+        self.markets = []
+        markets = r
+        for i, cont in enumerate(markets):
+            self.markets.append(markets[i]["MarketName"])
+        return self.markets
 
 
 app = webapp2.WSGIApplication([
